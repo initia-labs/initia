1e04b86547c453700152f13a57b06130d616cdab
beer-1 <147697694+beer-1@users.noreply.github.com>
2023-11-14 17:21:24 +0900

    update swagger

--- scripts/protoc-swagger-gen.sh
+++ scripts/protoc-swagger-gen.sh
@@ -6,14 +6,17 @@ set -eo pipefail
 COSMOS_URL=github.com/cosmos/cosmos-sdk
 IBC_URL=github.com/cosmos/ibc-go
 IBC_V=v7
+OPINIT_URL=github.com/initia-labs/OPinit
 
 COSMOS_SDK_VERSION=$(cat ./go.mod | grep "$COSMOS_URL v" | sed -n -e "s/^.* //p")
 IBC_VERSION=$(cat ./go.mod | grep "$IBC_URL/$IBC_V v" | sed -n -e "s/^.* //p")
+OPINIT_VERSION=$(cat ./go.mod | grep "$OPINIT_URL v" | sed -n -e "s/^.* //p")
 
 mkdir -p ./third_party
 cd third_party
-git clone -b $COSMOS_SDK_VERSION https://$COSMOS_URL
-git clone -b $IBC_VERSION https://$IBC_URL
+# git clone -b $COSMOS_SDK_VERSION https://$COSMOS_URL
+# git clone -b $IBC_VERSION https://$IBC_URL
+git clone -b $OPINIT_VERSION https://$OPINIT_URL
 cd ..
 
 # start generating
@@ -24,6 +27,7 @@ proto_dirs=$(find \
   ./ibc \
   ../third_party/cosmos-sdk/proto/cosmos \
   ../third_party/ibc-go/proto/ibc \
+  ../third_party/opinit/proto \
   -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   # generate swagger files (filter query files)
