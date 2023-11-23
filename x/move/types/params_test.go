f1a66536fc366e71929e1e9766debbf581fd3687
Vritra4 <Vritra@hitel.net>
2023-11-23 17:25:21 +0900

    remove max_module_size from param

--- x/move/types/params_test.go
+++ x/move/types/params_test.go
@@ -12,17 +12,31 @@ func TestParams(t *testing.T) {
 	p1 := DefaultParams()
 	require.NoError(t, p1.Validate())
 
-	p1.MaxModuleSize = 1000
-	err := p1.Validate()
-	require.Error(t, err)
+	p1.ContractSharedRevenueRatio = sdk.OneDec().Neg()
+	require.Error(t, p1.Validate())
 
 	p2 := DefaultParams()
 	p2.BaseDenom = "v#ksdjf"
-	err = p2.Validate()
+	err := p2.Validate()
 	require.Error(t, err)
 
 	p3 := DefaultParams()
 	p3.BaseMinGasPrice = sdk.OneDec().Neg()
 	err = p3.Validate()
 	require.Error(t, err)
+}
+
+func TestRawParams(t *testing.T) {
+	p1 := DefaultParams()
+	require.NoError(t, p1.Validate())
+
+	p1.ContractSharedRevenueRatio = sdk.OneDec()
+	p1.BaseDenom = "venusinthemorning"
+	p1.BaseMinGasPrice = sdk.OneDec()
+	require.NoError(t, p1.Validate())
+
+	rp := p1.ToRaw()
+	p2 := rp.ToParams(p1.ArbitraryEnabled)
+	require.NoError(t, p2.Validate())
+	require.Equal(t, p1, p2)
 }
