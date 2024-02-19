008f6acfbb7d4256a5684641e19f8c56b7b8a438
beer-1 <147697694+beer-1@users.noreply.github.com>
2024-02-19 18:58:39 +0900

    create ibc hooks module to enforce ACL

---
+++ x/ibc-hooks/move-hooks/timeout.go
@@ -0,0 +1,102 @@
+package move_hooks
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	transfertypes "github.com/cosmos/ibc-go/v8/modules/apps/transfer/types"
+	channeltypes "github.com/cosmos/ibc-go/v8/modules/core/04-channel/types"
+
+	ibchooks "github.com/initia-labs/initia/x/ibc-hooks"
+	nfttransfertypes "github.com/initia-labs/initia/x/ibc/nft-transfer/types"
+	movetypes "github.com/initia-labs/initia/x/move/types"
+	vmtypes "github.com/initia-labs/initiavm/types"
+)
+
+func (h MoveHooks) onTimeoutIcs20Packet(
+	ctx sdk.Context,
+	im ibchooks.IBCMiddleware,
+	packet channeltypes.Packet,
+	relayer sdk.AccAddress,
+	data transfertypes.FungibleTokenPacketData,
+) error {
+	if err := im.App.OnTimeoutPacket(ctx, packet, relayer); err != nil {
+		return err
+	}
+
+	isMoveRouted, hookData, err := validateAndParseMemo(data.GetMemo())
+	if !isMoveRouted || hookData.AsyncCallback == nil {
+		return nil
+	} else if err != nil {
+		return err
+	}
+
+	callback := hookData.AsyncCallback
+	if allowed, err := h.checkACL(im, ctx, callback.ModuleAddress); err != nil {
+		return err
+	} else if !allowed {
+		return nil
+	}
+
+	callbackIdBz, err := vmtypes.SerializeUint64(callback.Id)
+	if err != nil {
+		return err
+	}
+
+	_, err = h.execMsg(ctx, &movetypes.MsgExecute{
+		Sender:        data.Sender,
+		ModuleAddress: callback.ModuleAddress,
+		ModuleName:    callback.ModuleName,
+		FunctionName:  functionNameTimeout,
+		TypeArgs:      []string{},
+		Args:          [][]byte{callbackIdBz},
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (h MoveHooks) onTimeoutIcs721Packet(
+	ctx sdk.Context,
+	im ibchooks.IBCMiddleware,
+	packet channeltypes.Packet,
+	relayer sdk.AccAddress,
+	data nfttransfertypes.NonFungibleTokenPacketData,
+) error {
+	if err := im.App.OnTimeoutPacket(ctx, packet, relayer); err != nil {
+		return err
+	}
+
+	isMoveRouted, hookData, err := validateAndParseMemo(data.GetMemo())
+	if !isMoveRouted || hookData.AsyncCallback == nil {
+		return nil
+	} else if err != nil {
+		return err
+	}
+
+	callback := hookData.AsyncCallback
+	if allowed, err := h.checkACL(im, ctx, callback.ModuleAddress); err != nil {
+		return err
+	} else if !allowed {
+		return nil
+	}
+
+	callbackIdBz, err := vmtypes.SerializeUint64(callback.Id)
+	if err != nil {
+		return err
+	}
+
+	_, err = h.execMsg(ctx, &movetypes.MsgExecute{
+		Sender:        data.Sender,
+		ModuleAddress: callback.ModuleAddress,
+		ModuleName:    callback.ModuleName,
+		FunctionName:  functionNameTimeout,
+		TypeArgs:      []string{},
+		Args:          [][]byte{callbackIdBz},
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ x/ibc-hooks/types/keys.go
@@ -0,0 +1,25 @@
+package types
+
+const (
+	// ModuleName is the name of the hook module
+	ModuleName = "hook"
+
+	// StoreKey is the string store representation
+	StoreKey = ModuleName
+
+	// TStoreKey is the string transient store representation
+	TStoreKey = "transient_" + ModuleName
+
+	// QuerierRoute is the querier route for the hook module
+	QuerierRoute = ModuleName
+
+	// RouterKey is the msg router key for the hook module
+	RouterKey = ModuleName
+)
+
+// Keys for hook store
+// Items are stored with the following key: values
+var (
+	ACLPrefix = []byte{0x11} // prefix for allowed
+	ParamsKey = []byte{0x31} // prefix for parameters for module x/hook
+)
