4b27a983cb33e69cc006d4d26d06e439da6d50d1
beer-1 <147697694+beer-1@users.noreply.github.com>
2024-03-21 18:32:46 +0900

    add upgrade handler for v0.2.2

--- app/upgrade.go
+++ app/upgrade.go
@@ -1,10 +1,20 @@
 package app
 
 import (
+	"context"
+
+	upgradetypes "cosmossdk.io/x/upgrade/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 )
 
+const upgradeName = "v0.2.2"
+
 // RegisterUpgradeHandlers returns upgrade handlers
 func (app *InitiaApp) RegisterUpgradeHandlers(cfg module.Configurator) {
-	// no op
+	app.UpgradeKeeper.SetUpgradeHandler(
+		upgradeName,
+		func(ctx context.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+			return app.ModuleManager.RunMigrations(ctx, app.configurator, vm)
+		},
+	)
 }
