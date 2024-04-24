c63ca80394af0fffc4865acd0f201f0b289096e1
beer-1 <147697694+beer-1@users.noreply.github.com>
2024-04-24 17:04:29 +0900

    add burn test (#133)

--- x/move/keeper/bank_test.go
+++ x/move/keeper/bank_test.go
@@ -13,6 +13,8 @@ import (
 	cosmosbanktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 
 	"github.com/initia-labs/initia/x/move/keeper"
+	"github.com/initia-labs/initia/x/move/types"
+	vmtypes "github.com/initia-labs/movevm/types"
 )
 
 func Test_GetBalance(t *testing.T) {
@@ -175,4 +177,51 @@ func Test_GetMetadata(t *testing.T) {
 			Exponent: 6,
 		},
 	}, metadata.DenomUnits)
+}
+
+func Test_BurnCoins(t *testing.T) {
+	ctx, input := createDefaultTestInput(t)
+	moveBankKeeper := keeper.NewMoveBankKeeper(&input.MoveKeeper)
+
+	bz, err := hex.DecodeString("0000000000000000000000000000000000000002")
+	require.NoError(t, err)
+	twoAddr := sdk.AccAddress(bz)
+
+	amount := sdk.NewCoins(sdk.NewCoin("foo", sdkmath.NewIntFromUint64(1_000_000)))
+	input.Faucet.Fund(ctx, twoAddr, amount...)
+
+	// create token
+	err = input.MoveKeeper.ExecuteEntryFunctionJSON(
+		ctx,
+		vmtypes.TestAddress,
+		vmtypes.StdAddress,
+		"managed_coin",
+		"initialize",
+		[]vmtypes.TypeTag{},
+		[]string{`[]`, `"test coin"`, `"bar"`, `6`, `""`, `""`},
+	)
+	require.NoError(t, err)
+
+	// get bar metadata addr
+	barMetadata := types.NamedObjectAddress(vmtypes.TestAddress, "bar")
+	barDenom, err := types.DenomFromMetadataAddress(ctx, moveBankKeeper, barMetadata)
+	require.NoError(t, err)
+
+	// mint token
+	err = input.MoveKeeper.ExecuteEntryFunctionJSON(
+		ctx,
+		vmtypes.TestAddress,
+		vmtypes.StdAddress,
+		"managed_coin",
+		"mint",
+		[]vmtypes.TypeTag{},
+		[]string{fmt.Sprintf("\"%s\"", vmtypes.TestAddress.String()), fmt.Sprintf("\"%s\"", barMetadata.String()), `"1000000"`},
+	)
+	require.NoError(t, err)
+
+	err = moveBankKeeper.BurnCoins(ctx, twoAddr, sdk.NewCoins(sdk.NewCoin("foo", sdkmath.NewInt(500_000)), sdk.NewCoin(barDenom, sdkmath.NewInt(500_000))))
+	require.NoError(t, err)
+
+	require.Equal(t, sdk.NewCoin("foo", sdkmath.NewInt(500_000)), input.BankKeeper.GetBalance(ctx, twoAddr, "foo"))
+	require.Equal(t, sdk.NewCoin(barDenom, sdkmath.NewInt(500_000)), input.BankKeeper.GetBalance(ctx, twoAddr, barDenom))
 }
