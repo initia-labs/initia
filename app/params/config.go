502a87c8494d3e77b594c9d1d8737a36968a0d6a
beer-1 <147697694+beer-1@users.noreply.github.com>
2024-06-03 19:49:13 +0900

    remove unnecessary fork code

--- app/params/config.go
+++ app/params/config.go
@@ -1,11 +1,13 @@
 package params
 
 import (
+	"cosmossdk.io/x/tx/signing/aminojson"
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtx "github.com/cosmos/cosmos-sdk/x/auth/tx"
 
-	"github.com/initia-labs/initia/tx"
+	tx "github.com/initia-labs/initia/tx"
 )
 
 type config struct {
@@ -13,7 +15,21 @@ type config struct {
 }
 
 func NewClientTxConfig(protoCodec codec.ProtoCodecMarshaler) client.TxConfig {
-	return config{tx.NewTxConfig(protoCodec, tx.DefaultSignModes)}
+	signingOptions, err := authtx.NewDefaultSigningOptions()
+	if err != nil {
+		panic(err)
+	}
+
+	return config{
+		authtx.NewTxConfig(
+			protoCodec,
+			authtx.DefaultSignModes,
+			tx.NewSignModeEIP191Handler(aminojson.SignModeHandlerOptions{
+				FileResolver: signingOptions.FileResolver,
+				TypeResolver: signingOptions.TypeResolver,
+			}),
+		),
+	}
 }
 
 func (c config) TxDecoder() sdk.TxDecoder {
--- app/params/proto.go
+++ app/params/proto.go
@@ -3,10 +3,12 @@ package params
 import (
 	"cosmossdk.io/x/tx/signing"
 
+	"cosmossdk.io/x/tx/signing/aminojson"
 	"github.com/cosmos/cosmos-sdk/codec"
 	codecaddress "github.com/cosmos/cosmos-sdk/codec/address"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtx "github.com/cosmos/cosmos-sdk/x/auth/tx"
 	"github.com/cosmos/gogoproto/proto"
 
 	"github.com/initia-labs/initia/tx"
@@ -23,7 +25,19 @@ func MakeEncodingConfig() EncodingConfig {
 	})
 	appCodec := codec.NewProtoCodec(interfaceRegistry)
 	legacyAmino := codec.NewLegacyAmino()
-	txConfig := tx.NewTxConfig(appCodec, tx.DefaultSignModes)
+	signingOptions, err := authtx.NewDefaultSigningOptions()
+	if err != nil {
+		panic(err)
+	}
+
+	txConfig := authtx.NewTxConfig(
+		appCodec,
+		authtx.DefaultSignModes,
+		tx.NewSignModeEIP191Handler(aminojson.SignModeHandlerOptions{
+			FileResolver: signingOptions.FileResolver,
+			TypeResolver: signingOptions.TypeResolver,
+		}),
+	)
 
 	return EncodingConfig{
 		InterfaceRegistry: interfaceRegistry,
