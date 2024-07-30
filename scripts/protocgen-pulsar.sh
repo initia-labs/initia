4b9a57a2bf56e9091bbcc5956d5afba1eaefa387
beer-1 <147697694+beer-1@users.noreply.github.com>
2024-07-30 13:03:55 +0900

    bump opinit and cleanup template

--- scripts/protocgen-pulsar.sh
+++ scripts/protocgen-pulsar.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 protoc_install_gopulsar() {
   go install github.com/cosmos/cosmos-proto/cmd/protoc-gen-go-pulsar@latest
-  go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+  go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28.1
 }
 
 protoc_install_gopulsar
