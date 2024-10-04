02b3968858737b277a441bdbc5ada23ae43521a4
beer-1 <147697694+beer-1@users.noreply.github.com>
2024-10-04 19:45:37 +0900

    add ordering at ica account creation (#276)

--- x/intertx/client/cli/flags.go
+++ x/intertx/client/cli/flags.go
@@ -9,15 +9,19 @@ const (
 	FlagConnectionID = "connection-id"
 	// The controller chain channel version
 	FlagVersion = "version"
+	// Set the ordering of the channel to ordered
+	FlagOrdered = "ordered"
 )
 
 // common flagsets to add to various functions
 var (
 	fsConnectionID = flag.NewFlagSet("", flag.ContinueOnError)
 	fsVersion      = flag.NewFlagSet("", flag.ContinueOnError)
+	fsOrdered      = flag.NewFlagSet("", flag.ContinueOnError)
 )
 
 func init() {
 	fsConnectionID.String(FlagConnectionID, "", "Connection ID")
 	fsVersion.String(FlagVersion, "", "Version")
+	fsOrdered.Bool(FlagOrdered, false, "Set the ordering of the channel to ordered")
 }
