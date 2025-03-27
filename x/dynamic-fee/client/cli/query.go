5519f6393147f9c1d83b329a181c7d0dec2c4a8d
sh-cha <snghuncha@gmail.com>
2025-03-27 15:15:17 +0900

    add custom query to dynamic-fee

---
+++ x/dynamic-fee/client/cli/query.go
@@ -0,0 +1,66 @@
+package cli
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+
+	"cosmossdk.io/core/address"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/version"
+
+	"github.com/initia-labs/initia/x/dynamic-fee/types"
+)
+
+func GetQueryCmd(ac address.Codec) *cobra.Command {
+	queryCmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      "Querying commands for the dynamic-fee module",
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+	queryCmd.AddCommand(
+		GetCmdQueryParams(),
+	)
+	return queryCmd
+}
+
+// GetCmdQueryParams implements the params query command.
+func GetCmdQueryParams() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "params",
+		Args:  cobra.NoArgs,
+		Short: "Query the current dynamic-fee parameters information",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Query values set as dynamic-fee parameters.
+
+Example:
+$ %s query dynamicfee params
+`,
+				version.AppName,
+			),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			queryClient := types.NewQueryClient(clientCtx)
+
+			res, err := queryClient.Params(cmd.Context(), &types.QueryParamsRequest{})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(&res.Params)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
