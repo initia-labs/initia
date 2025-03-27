e743cfee75b346e8b778c0f5082eee6b1350ea72
sh-cha <snghuncha@gmail.com>
2025-03-27 14:12:29 +0900

    add dynamic fee module to upgrade handler

--- app/upgrade.go
+++ app/upgrade.go
@@ -3,9 +3,13 @@ package app
 import (
 	"context"
 
+	"cosmossdk.io/errors"
+	"cosmossdk.io/math"
 	storetypes "cosmossdk.io/store/types"
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+
+	dynamicfeetypes "github.com/initia-labs/initia/x/dynamic-fee/types"
 )
 
 const upgradeName = "v1.0.0-rc.0"
@@ -19,7 +23,9 @@ func (app *InitiaApp) RegisterUpgradeHandlers(cfg module.Configurator) {
 
 	if upgradeInfo.Name == upgradeName && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
 		storeUpgrades := storetypes.StoreUpgrades{
-			Added: []string{}, // add dynamic fee store
+			Added: []string{
+				dynamicfeetypes.StoreKey,
+			},
 		}
 
 		// configure store loader that checks if version == upgradeHeight and applies store upgrades
@@ -29,7 +35,14 @@ func (app *InitiaApp) RegisterUpgradeHandlers(cfg module.Configurator) {
 	app.UpgradeKeeper.SetUpgradeHandler(
 		upgradeName,
 		func(ctx context.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
-			return app.ModuleManager.RunMigrations(ctx, cfg, vm)
+			params := dynamicfeetypes.DefaultParams()
+			params.MaxChangeRate = math.LegacyZeroDec()
+
+			err := app.DynamicFeeKeeper.SetParams(ctx, params)
+			if err != nil {
+				return nil, errors.Wrap(err, "failed to set dynamic fee params")
+			}
+			return vm, nil
 		},
 	)
 }
