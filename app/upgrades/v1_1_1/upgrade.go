ad6b7df5e2400800761f99dc9985cefb17e216bd
beer-1 <beer-1@codingcats.xyz>
2025-05-25 00:57:42 +0900

    fix to distribute only INIT token && register proper upgrade handler with v1.1.1

---
+++ app/upgrades/v1_1_1/upgrade.go
@@ -0,0 +1,48 @@
+package v1_1_1
+
+import (
+	"context"
+	"encoding/base64"
+
+	upgradetypes "cosmossdk.io/x/upgrade/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+
+	"github.com/initia-labs/initia/app/upgrades"
+	movetypes "github.com/initia-labs/initia/x/move/types"
+
+	vmtypes "github.com/initia-labs/movevm/types"
+)
+
+const upgradeName = "v1.1.1"
+
+// RegisterUpgradeHandlers returns upgrade handlers
+func RegisterUpgradeHandlers(app upgrades.InitiaApp) {
+	app.GetUpgradeKeeper().SetUpgradeHandler(
+		upgradeName,
+		func(ctx context.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+			// upgrade 0x1::primary_fungible_store, 0x1::fungible_asset, 0x1::dispatchable_fungible_asset modules
+			// - https://github.com/initia-labs/movevm/pull/207
+
+			moduleBytesArray := make([][]byte, len(moduleBytes))
+			var err error
+			for i, c := range moduleBytes {
+				moduleBytesArray[i], err = base64.StdEncoding.DecodeString(c)
+				if err != nil {
+					return nil, err
+				}
+			}
+
+			var modules []vmtypes.Module
+			for _, module := range moduleBytesArray {
+				modules = append(modules, vmtypes.NewModule(module))
+			}
+
+			err = app.GetMoveKeeper().PublishModuleBundle(ctx, vmtypes.StdAddress, vmtypes.NewModuleBundle(modules...), movetypes.UpgradePolicy_COMPATIBLE)
+			if err != nil {
+				return nil, err
+			}
+
+			return vm, nil
+		},
+	)
+}
