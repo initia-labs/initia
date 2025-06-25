b22fad87af10088eb14eab7238aa6c300aaa7e88
beer-1 <beer-1@codingcats.xyz>
2025-06-25 14:40:21 +0900

    fix typo

--- crypto/codec/codec.go
+++ crypto/codec/codec.go
@@ -8,7 +8,7 @@ import (
 	"github.com/initia-labs/initia/crypto/ethsecp256k1"
 )
 
-// RegisterInterfaces register the Evmos key concrete types.
+// RegisterInterfaces register the Initia key concrete types.
 func RegisterInterfaces(registry codectypes.InterfaceRegistry) {
 	registry.RegisterImplementations((*cryptotypes.PubKey)(nil), &ethsecp256k1.PubKey{})
 	registry.RegisterImplementations((*cryptotypes.PrivKey)(nil), &ethsecp256k1.PrivKey{})
