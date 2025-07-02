83a0c0cc8ddeb89d97b337a3dbc7c80c1271caf0
beer-1 <beer-1@codingcats.xyz>
2025-07-02 19:30:56 +0900

    minimize fork code

--- x/authz/client/cli/tx.go
+++ x/authz/client/cli/tx.go
@@ -2,7 +2,6 @@ package cli
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"os"
 	"strings"
@@ -12,41 +11,29 @@ import (
 	"github.com/spf13/cobra"
 
 	movetypes "github.com/initia-labs/initia/x/move/types"
-	mstaking "github.com/initia-labs/initia/x/mstaking/types"
 
 	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/tx"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/version"
-	authclient "github.com/cosmos/cosmos-sdk/x/auth/client"
 	"github.com/cosmos/cosmos-sdk/x/authz"
+	authzcli "github.com/cosmos/cosmos-sdk/x/authz/client/cli"
 	bank "github.com/cosmos/cosmos-sdk/x/bank/types"
 )
 
 // Flag names and valuesi
 const (
-	FlagSpendLimit        = "spend-limit"
-	FlagMsgType           = "msg-type"
-	FlagExpiration        = "expiration"
-	FlagAllowedValidators = "allowed-validators"
-	FlagDenyValidators    = "deny-validators"
-	FlagAllowList         = "allow-list"
-	FlagType              = "type"
-	FlagItems             = "items"
-	FlagModules           = "modules"
-	generic               = "generic"
-	delegate              = "delegate"
-	redelegate            = "redelegate"
-	unbond                = "unbond"
-	move                  = "move"
-	publish               = "publish"
-	execute               = "execute"
+	FlagExpiration = "expiration"
+	FlagType       = "type"
+	FlagItems      = "items"
+	FlagModules    = "modules"
+	move           = "move"
+	publish        = "publish"
+	execute        = "execute"
 )
 
 // GetTxCmd returns the transaction commands for this module
 func GetTxCmd(ac, vc address.Codec) *cobra.Command {
-	AuthorizationTxCmd := &cobra.Command{
+	txCmd := &cobra.Command{
 		Use:                        authz.ModuleName,
 		Short:                      "Authorization transactions subcommands",
 		Long:                       "Authorize and revoke access to execute transactions on behalf of your address",
@@ -55,16 +42,17 @@ func GetTxCmd(ac, vc address.Codec) *cobra.Command {
 		RunE:                       client.ValidateCmd,
 	}
 
-	AuthorizationTxCmd.AddCommand(
+	txCmd.AddCommand(
 		NewCmdGrantAuthorization(ac, vc),
-		NewCmdRevokeAuthorization(ac),
-		NewCmdExecAuthorization(),
+		authzcli.NewCmdRevokeAuthorization(ac),
+		authzcli.NewCmdExecAuthorization(),
 	)
 
-	return AuthorizationTxCmd
+	return txCmd
 }
 
 func NewCmdGrantAuthorization(ac, vc address.Codec) *cobra.Command {
+	originCmd := authzcli.NewCmdGrantAuthorization(ac)
 	cmd := &cobra.Command{
 		Use:   "grant <grantee> <authorization_type=\"send\"|\"generic\"|\"delegate\"|\"unbond\"|\"redelegate\"|\"move\"> --from <granter>",
 		Short: "Grant authorization to an address",
@@ -94,6 +82,11 @@ Where authzItems.json contains:
 		),
 		Args: cobra.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
+			// If not a Move authorization, delegate to standard authz CLI handler
+			if args[1] != move {
+				return originCmd.RunE(cmd, args)
+			}
+
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
 				return err
@@ -105,136 +98,47 @@ Where authzItems.json contains:
 			}
 
 			var authorization authz.Authorization
-			switch args[1] {
-			case "send":
-				limit, err := cmd.Flags().GetString(FlagSpendLimit)
+			typ, err := cmd.Flags().GetString(FlagType)
+			if err != nil {
+				return err
+			}
+
+			switch typ {
+			case publish:
+				var modpatstr string
+				modpatstr, err = cmd.Flags().GetString(FlagModules)
 				if err != nil {
 					return err
 				}
-
-				spendLimit, err := sdk.ParseCoinsNormalized(limit)
+				if modpatstr == "" {
+					return fmt.Errorf("module pattern is empty")
+				}
+				modulePatterns := strings.Split(modpatstr, ",")
+				authorization, err = movetypes.NewPublishAuthorization(modulePatterns)
+			case execute:
+				var items string
+				items, err = cmd.Flags().GetString(FlagItems)
 				if err != nil {
 					return err
 				}
-
-				if !spendLimit.IsAllPositive() {
-					return fmt.Errorf("spend-limit should be greater than zero")
+				if items == "" {
+					return fmt.Errorf("items file path is empty")
 				}
-
-				allowList, err := cmd.Flags().GetStringSlice(FlagAllowList)
+				var itemsBytes []byte
+				itemsBytes, err = os.ReadFile(items)
 				if err != nil {
 					return err
 				}
-
-				allowed, err := bech32toAccAddresses(allowList, ac)
-				if err != nil {
-					return err
+				var authzItems []movetypes.ExecuteAuthorizationItem
+				if err = json.Unmarshal(itemsBytes, &authzItems); err != nil {
+					return fmt.Errorf("invalid authorization item, %s", items)
 				}
-
-				authorization = bank.NewSendAuthorization(spendLimit, allowed)
-			case generic:
-				msgType, err := cmd.Flags().GetString(FlagMsgType)
-				if err != nil {
-					return err
-				}
-
-				authorization = authz.NewGenericAuthorization(msgType)
-			case delegate, unbond, redelegate:
-				limit, err := cmd.Flags().GetString(FlagSpendLimit)
-				if err != nil {
-					return err
-				}
-
-				allowValidators, err := cmd.Flags().GetStringSlice(FlagAllowedValidators)
-				if err != nil {
-					return err
-				}
-
-				denyValidators, err := cmd.Flags().GetStringSlice(FlagDenyValidators)
-				if err != nil {
-					return err
-				}
-
-				var delegateLimit sdk.Coins
-				if limit != "" {
-					spendLimit, err := sdk.ParseCoinsNormalized(limit)
-					if err != nil {
-						return err
-					}
-
-					if !spendLimit.IsAllPositive() {
-						return fmt.Errorf("spend-limit should be greater than zero")
-					}
-					delegateLimit = spendLimit
-				}
-
-				_, err = bech32toValidatorAddresses(allowValidators, vc)
-				if err != nil {
-					return err
-				}
-
-				_, err = bech32toValidatorAddresses(denyValidators, vc)
-				if err != nil {
-					return err
-				}
-
-				switch args[1] {
-				case delegate:
-					authorization, err = mstaking.NewStakeAuthorization(allowValidators, denyValidators, mstaking.AuthorizationType_AUTHORIZATION_TYPE_DELEGATE, delegateLimit)
-				case unbond:
-					authorization, err = mstaking.NewStakeAuthorization(allowValidators, denyValidators, mstaking.AuthorizationType_AUTHORIZATION_TYPE_UNDELEGATE, delegateLimit)
-				default:
-					authorization, err = mstaking.NewStakeAuthorization(allowValidators, denyValidators, mstaking.AuthorizationType_AUTHORIZATION_TYPE_REDELEGATE, delegateLimit)
-				}
-				if err != nil {
-					return err
-				}
-			case move:
-				typ, err := cmd.Flags().GetString(FlagType)
-				if err != nil {
-					return err
-				}
-
-				switch typ {
-				case publish:
-					var modpatstr string
-					modpatstr, err = cmd.Flags().GetString(FlagModules)
-					if err != nil {
-						return err
-					}
-					if modpatstr == "" {
-						return fmt.Errorf("module pattern is empty")
-					}
-					modulePatterns := strings.Split(modpatstr, ",")
-					authorization, err = movetypes.NewPublishAuthorization(modulePatterns)
-				case execute:
-					var items string
-					items, err = cmd.Flags().GetString(FlagItems)
-					if err != nil {
-						return err
-					}
-					if items == "" {
-						return fmt.Errorf("items file path is empty")
-					}
-					var itemsBytes []byte
-					itemsBytes, err = os.ReadFile(items)
-					if err != nil {
-						return err
-					}
-					var authzItems []movetypes.ExecuteAuthorizationItem
-					if err = json.Unmarshal(itemsBytes, &authzItems); err != nil {
-						return fmt.Errorf("invalid authorization item, %s", items)
-					}
-					authorization, err = movetypes.NewExecuteAuthorization(ac, authzItems)
-				default:
-					return fmt.Errorf("invalid type, %s", typ)
-				}
-				if err != nil {
-					return err
-				}
-
+				authorization, err = movetypes.NewExecuteAuthorization(ac, authzItems)
 			default:
-				return fmt.Errorf("invalid authorization type, %s", args[1])
+				return fmt.Errorf("invalid type, %s", typ)
+			}
+			if err != nil {
+				return err
 			}
 
 			expire, err := getExpireTime(cmd)
@@ -250,13 +154,8 @@ Where authzItems.json contains:
 			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
 		},
 	}
-	flags.AddTxFlagsToCmd(cmd)
-	cmd.Flags().String(FlagMsgType, "", "The Msg method name for which we are creating a GenericAuthorization")
-	cmd.Flags().String(FlagSpendLimit, "", "SpendLimit for Send Authorization, an array of Coins allowed spend")
-	cmd.Flags().StringSlice(FlagAllowedValidators, []string{}, "Allowed validators addresses separated by ,")
-	cmd.Flags().StringSlice(FlagDenyValidators, []string{}, "Deny validators addresses separated by ,")
-	cmd.Flags().StringSlice(FlagAllowList, []string{}, "Allowed addresses grantee is allowed to send funds separated by ,")
-	cmd.Flags().Int64(FlagExpiration, time.Now().UTC().AddDate(1, 0, 0).Unix(), "The Unix timestamp. Default is one year.")
+
+	cmd.Flags().AddFlagSet(originCmd.Flags())
 	cmd.Flags().String(FlagType, "", "The type of move authorization, {publish|execute}")
 	cmd.Flags().String(FlagItems, "", "The items of move execute authorization, a json file path.")
 	cmd.Flags().String(FlagModules, "", "The items of move publish authorization, a comma-separated string of module names.")
@@ -273,100 +172,4 @@ func getExpireTime(cmd *cobra.Command) (*time.Time, error) {
 	}
 	e := time.Unix(exp, 0)
 	return &e, nil
-}
-
-func NewCmdRevokeAuthorization(ac address.Codec) *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "revoke [grantee] [msg_type] --from=[granter]",
-		Short: "revoke authorization",
-		Long: strings.TrimSpace(
-			fmt.Sprintf(`revoke authorization from a granter to a grantee:
-Example:
- $ %s tx %s revoke init1vrit.. %s --from=init1vrit..
-            `, version.AppName, authz.ModuleName, bank.SendAuthorization{}.MsgTypeURL()),
-		),
-		Args: cobra.ExactArgs(2),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			clientCtx, err := client.GetClientTxContext(cmd)
-			if err != nil {
-				return err
-			}
-
-			grantee, err := ac.StringToBytes(args[0])
-			if err != nil {
-				return err
-			}
-
-			granter := clientCtx.GetFromAddress()
-			msgAuthorized := args[1]
-			msg := authz.NewMsgRevoke(granter, grantee, msgAuthorized)
-
-			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
-		},
-	}
-	flags.AddTxFlagsToCmd(cmd)
-	return cmd
-}
-
-func NewCmdExecAuthorization() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "exec [msg_tx_json_file] --from [grantee]",
-		Short: "execute tx on behalf of granter account",
-		Long: strings.TrimSpace(
-			fmt.Sprintf(`execute tx on behalf of granter account:
-Example:
- $ %s tx %s exec tx.json --from grantee
- $ %s tx bank send <granter> <recipient> --from <granter> --chain-id <chain-id> --generate-only > tx.json && %s tx %s exec tx.json --from grantee
-            `, version.AppName, authz.ModuleName, version.AppName, version.AppName, authz.ModuleName),
-		),
-		Args: cobra.ExactArgs(1),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			clientCtx, err := client.GetClientTxContext(cmd)
-			if err != nil {
-				return err
-			}
-			grantee := clientCtx.GetFromAddress()
-
-			if offline, _ := cmd.Flags().GetBool(flags.FlagOffline); offline {
-				return errors.New("cannot broadcast tx during offline mode")
-			}
-
-			theTx, err := authclient.ReadTxFromFile(clientCtx, args[0])
-			if err != nil {
-				return err
-			}
-			msg := authz.NewMsgExec(grantee, theTx.GetMsgs())
-
-			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
-		},
-	}
-
-	flags.AddTxFlagsToCmd(cmd)
-
-	return cmd
-}
-
-func bech32toValidatorAddresses(validators []string, vc address.Codec) ([]sdk.ValAddress, error) {
-	vals := make([]sdk.ValAddress, len(validators))
-	for i, addr := range validators {
-		valAddr, err := vc.StringToBytes(addr)
-		if err != nil {
-			return nil, err
-		}
-		vals[i] = valAddr
-	}
-	return vals, nil
-}
-
-// bech32toAccAddresses returns []AccAddress from a list of Bech32 string addresses.
-func bech32toAccAddresses(accAddrs []string, ac address.Codec) ([]sdk.AccAddress, error) {
-	addrs := make([]sdk.AccAddress, len(accAddrs))
-	for i, addr := range accAddrs {
-		accAddr, err := ac.StringToBytes(addr)
-		if err != nil {
-			return nil, err
-		}
-		addrs[i] = accAddr
-	}
-	return addrs, nil
 }
--- x/authz/module/module.go
+++ x/authz/module/module.go
@@ -1,140 +1,37 @@
 package authz
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
-
-	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/spf13/cobra"
 
-	"cosmossdk.io/core/appmodule"
-	"cosmossdk.io/errors"
-
-	sdkclient "github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/codec"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/types/module"
 	"github.com/cosmos/cosmos-sdk/x/authz"
 	"github.com/cosmos/cosmos-sdk/x/authz/keeper"
+	authzmodule "github.com/cosmos/cosmos-sdk/x/authz/module"
 
 	"github.com/initia-labs/initia/x/authz/client/cli"
 )
 
-const ConsensusVersion = 1
-
-var (
-	_ module.AppModuleBasic = AppModule{}
-	_ module.HasGenesis     = AppModule{}
-	_ module.HasServices    = AppModule{}
-
-	_ appmodule.AppModule       = AppModule{}
-	_ appmodule.HasBeginBlocker = AppModule{}
-)
-
-// AppModuleBasic defines the basic application module used by the authz module.
-type AppModuleBasic struct {
+// AppModule defines the authz module.
+type AppModule struct {
+	authzmodule.AppModule
 	cdc codec.Codec
 }
 
-// Name returns the authz module's name.
-func (AppModuleBasic) Name() string {
-	return authz.ModuleName
-}
-
-// RegisterServices registers a gRPC query service to respond to the
-// module-specific gRPC queries.
-func (am AppModule) RegisterServices(cfg module.Configurator) {
-	authz.RegisterQueryServer(cfg.QueryServer(), am.keeper)
-	authz.RegisterMsgServer(cfg.MsgServer(), am.keeper)
-}
-
-// RegisterLegacyAminoCodec registers the authz module's types for the given codec.
-func (AppModuleBasic) RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {}
-
-// RegisterInterfaces registers the authz module's interface types
-func (AppModuleBasic) RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
-	authz.RegisterInterfaces(registry)
-}
-
-// DefaultGenesis returns default genesis state as raw bytes for the authz
-// module.
-func (AppModuleBasic) DefaultGenesis(cdc codec.JSONCodec) json.RawMessage {
-	return cdc.MustMarshalJSON(authz.DefaultGenesisState())
-}
-
-// ValidateGenesis performs genesis state validation for the authz module.
-func (AppModuleBasic) ValidateGenesis(cdc codec.JSONCodec, config sdkclient.TxEncodingConfig, bz json.RawMessage) error {
-	var data authz.GenesisState
-	if err := cdc.UnmarshalJSON(bz, &data); err != nil {
-		return errors.Wrapf(err, "failed to unmarshal %s genesis state", authz.ModuleName)
-	}
-
-	return authz.ValidateGenesis(data)
-}
-
-// RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the authz module.
-func (a AppModuleBasic) RegisterGRPCGatewayRoutes(clientCtx sdkclient.Context, mux *runtime.ServeMux) {
-	if err := authz.RegisterQueryHandlerClient(context.Background(), mux, authz.NewQueryClient(clientCtx)); err != nil {
-		panic(fmt.Sprintf("could not register gRPC query service: %v", err))
-	}
-}
-
-// GetTxCmd returns the transaction commands for the authz module
-func (ab AppModuleBasic) GetTxCmd() *cobra.Command {
-	return cli.GetTxCmd(ab.cdc.InterfaceRegistry().SigningContext().AddressCodec(), ab.cdc.InterfaceRegistry().SigningContext().ValidatorAddressCodec())
-}
-
-// AppModule implements the sdk.AppModule interface
-type AppModule struct {
-	AppModuleBasic
-	keeper   keeper.Keeper
-	registry cdctypes.InterfaceRegistry
-}
-
-// NewAppModule creates a new AppModule object
-func NewAppModule(cdc codec.Codec, keeper keeper.Keeper, registry cdctypes.InterfaceRegistry) AppModule {
+// NewAppModule returns a new AppModule.
+func NewAppModule(cdc codec.Codec, keeper keeper.Keeper, accountKeeper authz.AccountKeeper, bankKeeper authz.BankKeeper, interfaceRegistry cdctypes.InterfaceRegistry) AppModule {
 	return AppModule{
-		AppModuleBasic: AppModuleBasic{cdc: cdc},
-		keeper:         keeper,
-		registry:       registry,
+		AppModule: authzmodule.NewAppModule(cdc, keeper, accountKeeper, bankKeeper, interfaceRegistry),
+		cdc:       cdc,
 	}
 }
 
-// Name returns the authz module's name.
-func (AppModule) Name() string {
-	return authz.ModuleName
-}
-
-// IsOnePerModuleType implements the depinject.OnePerModuleType interface.
-func (am AppModule) IsOnePerModuleType() {}
-
-// IsAppModule implements the appmodule.AppModule interface.
-func (am AppModule) IsAppModule() {}
-
-// RegisterInvariants does nothing, there are no invariants to enforce
-func (AppModule) RegisterInvariants(_ sdk.InvariantRegistry) {}
-
-// InitGenesis performs genesis initialization for the authz module. It returns
-// no validator updates.
-func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONCodec, data json.RawMessage) {
-	var genesisState authz.GenesisState
-	cdc.MustUnmarshalJSON(data, &genesisState)
-	am.keeper.InitGenesis(ctx, &genesisState)
-}
-
-// ExportGenesis returns the exported genesis state as raw bytes for the authz
-// module.
-func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.RawMessage {
-	gs := am.keeper.ExportGenesis(ctx)
-	return cdc.MustMarshalJSON(gs)
-}
-
-// ConsensusVersion implements AppModule/ConsensusVersion.
-func (AppModule) ConsensusVersion() uint64 { return ConsensusVersion }
-
-// BeginBlock returns the begin blocker for the authz module.
-func (am AppModule) BeginBlock(ctx context.Context) error {
-	return BeginBlocker(ctx, am.keeper)
+// GetTxCmd returns custom tx commands for the authz module.
+//
+// NOTE: we are using autocli style to register tx commands.
+func (ab AppModule) GetTxCmd() *cobra.Command {
+	return cli.GetTxCmd(
+		ab.cdc.InterfaceRegistry().SigningContext().AddressCodec(),
+		ab.cdc.InterfaceRegistry().SigningContext().ValidatorAddressCodec(),
+	)
 }
