0f14141934075e24c1455a1510ea66feb39861e1
beer-1 <beer-1@codingcats.xyz>
2025-09-23 13:51:44 +0900

    register proto

---
+++ x/move/autocli.go
@@ -0,0 +1,23 @@
+package move
+
+import (
+	autocliv1 "cosmossdk.io/api/cosmos/autocli/v1"
+
+	movev1 "github.com/initia-labs/initia/api/initia/move/v1"
+)
+
+// AutoCLIOptions implements the autocli.HasAutoCLIConfig interface.
+func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
+	return &autocliv1.ModuleOptions{
+		Query: &autocliv1.ServiceCommandDescriptor{
+			Service:              movev1.Query_ServiceDesc.ServiceName,
+			RpcCommandOptions:    []*autocliv1.RpcCommandOptions{},
+			EnhanceCustomCommand: true,
+		},
+		Tx: &autocliv1.ServiceCommandDescriptor{
+			Service:              movev1.Msg_ServiceDesc.ServiceName,
+			RpcCommandOptions:    []*autocliv1.RpcCommandOptions{},
+			EnhanceCustomCommand: true,
+		},
+	}
+}
