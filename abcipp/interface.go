59975156c9658fdd297a023596aab9aae4c4a651
beer-1 <beer-1@codingcats.xyz>
2026-01-29 20:04:28 +0900

    fix race

--- abcipp/interface.go
+++ abcipp/interface.go
@@ -50,4 +50,5 @@ type CheckTx func(req *cometabci.RequestCheckTx) (*cometabci.ResponseCheckTx, er
 // as well as retrieve the latest committed state.
 type BaseApp interface {
 	GetContextForCheckTx(txBytes []byte) sdk.Context
+	IsSealed() bool
 }
