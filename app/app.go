76d0e5ff90af2fec72ad8892fb5854131f8591ad
beer-1 <beer-1@codingcats.xyz>
2026-02-04 14:34:48 +0900

    use latest upgrade

--- app/app.go
+++ app/app.go
@@ -57,7 +57,7 @@ import (
 	"github.com/initia-labs/initia/abcipp"
 	"github.com/initia-labs/initia/app/keepers"
 	"github.com/initia-labs/initia/app/params"
-	upgrades_v1_3_0 "github.com/initia-labs/initia/app/upgrades/v1_3_0"
+	upgrades_v1_4_0 "github.com/initia-labs/initia/app/upgrades/v1_4_0"
 	cryptocodec "github.com/initia-labs/initia/crypto/codec"
 	initiatx "github.com/initia-labs/initia/tx"
 	moveconfig "github.com/initia-labs/initia/x/move/config"
@@ -247,7 +247,7 @@ func NewInitiaApp(
 	// The cosmos upgrade handler attempts to create ${HOME}/.initia/data to check for upgrade info,
 	// but this isn't required during initial encoding config setup.
 	if loadLatest {
-		upgrades_v1_3_0.RegisterUpgradeHandlers(app)
+		upgrades_v1_4_0.RegisterUpgradeHandlers(app)
 	}
 
 	autocliv1.RegisterQueryServer(app.GRPCQueryRouter(), runtimeservices.NewAutoCLIQueryService(app.ModuleManager.Modules))
