e3875c81375e77331ebfa71eb2ce0949e3349f2c
beer-1 <beer-1@codingcats.xyz>
2026-04-21 10:51:11 +0900

    fix benchmark to correctly measure time

--- crypto/ethsecp256k1/benchmark_test.go
+++ crypto/ethsecp256k1/benchmark_test.go
@@ -16,14 +16,15 @@ func BenchmarkPubKey_VerifySignature(b *testing.B) {
 	privKey := GenerateKey()
 	pubKey := privKey.PubKey()
 
-	b.ResetTimer()
 	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
+	for i := 0; b.Loop(); i++ {
+		b.StopTimer()
 		msg := fmt.Appendf(nil, "%10d", i)
 		sig, err := privKey.Sign(msg)
 		if err != nil {
 			b.Fatal(err)
 		}
+		b.StartTimer()
 		pubKey.VerifySignature(msg, sig)
 	}
 }
